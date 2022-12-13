cab71fbfabd4bd052364893429d22631d83e3f75
jbep <joshuabemartin@gmail.com>
2022-12-13 16:51:45 -0500

    recursive chapter

---
+++ recursive.js
@@ -0,0 +1,9 @@
+//3.1
+//the greet function is called with maggie
+//then greet 2 is claled, but greet is left in a  suspending state until greet2 is done
+//once greet2 is finished we return to greet
+//3.2
+//it is full of partially finished functions
+//Answer: The stack grows forever. Each program has a limited
+//amount of space on the call stack. When your program runs
+//out of space (which it eventually will), it will exit with a stackoverflow error.
