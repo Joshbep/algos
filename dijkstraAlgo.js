c5398d686915a7e0371b6e9a3d1e581d906c5ae4
jbep <joshuabemartin@gmail.com>
2022-12-21 16:00:37 -0500

    Dijkstra’s algorithm and more examples

---
+++ dijkstraAlgo.js
@@ -0,0 +1,14 @@
+//BFS is shortest path doesn't mean fastest
+//if you want to find the fastest you can use a different algorithm called Dijkstra’s algorithm.
+//with Dijkstra’s algorithm each edge in a graph has a number associated with it which is a weight
+//a graph with weights is called a weighted graph
+//A graph without weights is called an unweighted graph.
+
+// if it is unweighted to calculate the shortest path use breadth first search
+
+//to calculate the shortest path in a weighted graph use Dijkstra’s algorithm
+
+//an undirected graph means that both notes point to each other
+//thats a cycle!
+
+//Dijkstra’s algorithm only works with directed acyclic graphs, called DAGs for short.
