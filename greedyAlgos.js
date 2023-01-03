46dd2a46ab473aa6420dcf90c0d683b67b5c9b0a
jbep <joshuabemartin@gmail.com>
2023-01-03 16:22:01 -0500

    8.1 and 8.2 exercies

---
+++ greedyAlgos.js
@@ -0,0 +1,9 @@
+//greedy algos are an algorithm that solves the
+//problem pretty well. And that’s where greedy algorithms shine, because
+//they’re simple to write and usually get pretty close.
+
+//8.1
+//smallest boxes first and keep moving up
+//8.2
+//go to the highest ranked places
+//
