484375fabb2e4bf10606449fec915e87d1d5c8ca
jbep <joshuabemartin@gmail.com>
2023-01-03 16:23:02 -0500

    8.1 and 8.2 exercies

--- greedyAlgos.js
+++ greedyAlgos.js
@@ -3,7 +3,10 @@
 //they’re simple to write and usually get pretty close.
 
 //8.1
-//smallest boxes first and keep moving up
+//Answer: A greedy strategy would be to pick the largest box that
+//will fit in the remaining space, and repeat until you can’t pack any
+//more boxes. No, this won’t give you the optimal solution.
 //8.2
-//go to the highest ranked places
-//
+//Answer: Keep picking the activity with the highest point value that
+//you can still do in the time you have left. Stop when you can’t do
+//anything else. No, this won’t give you the optimal solution.
