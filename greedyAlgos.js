8ac6c3219d568fcf397141975f1d3244a0d38518
jbep <joshuabemartin@gmail.com>
2023-01-04 15:51:21 -0500

    hoe to tell NP-Complete

--- greedyAlgos.js
+++ greedyAlgos.js
@@ -19,3 +19,19 @@
 // Greedy
 //8.5
 //Greedy
+
+//there’s no easy way to tell if the problem you’re working
+//on is NP-complete. Here are some giveaways:
+//• Your algorithm runs quickly with a handful of items but really slows
+//down with more items.
+//• “All combinations of X” usually point to an NP-complete problem.
+//• Do you have to calculate “every possible version” of X because you
+//can’t break it down into smaller sub-problems? Might be
+//NP-complete.
+//• If your problem involves a sequence (such as a sequence of cities, like
+//traveling salesperson), and it’s hard to solve, it might be NP-complete.
+//• If your problem involves a set (like a set of radio stations) and it’s hard
+//to solve, it might be NP-complete.
+//• Can you restate your problem as the set-covering problem or the
+//traveling-salesperson problem? Then your problem is definitely
+//NP-complete.
