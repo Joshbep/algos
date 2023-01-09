4f78b9814f8d6235df71855d3dffea7a4c12d796
jbep <joshuabemartin@gmail.com>
2023-01-09 18:50:54 -0500

    notes

--- dynamicProgramming.js
+++ dynamicProgramming.js
@@ -2,3 +2,11 @@
 //9.1
 //Answer: Yes. Then you could steal the MP3 player, the iPhone, and
 //the guitar, worth a total of $4,500.
+
+
+//order or rows doesn't matter .
+
+
+//With the dynamic-programming solution, you
+//either take the item or not. There’s no way for it to figure out that you
+//should take half an item.
