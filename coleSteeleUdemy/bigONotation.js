0ef64f09aee82c82c1f4f0996451ec7fd1281ec0
jbep <joshuabemartin@gmail.com>
2023-01-12 12:43:19 -0500

    started taking notes on Big O

---
+++ coleSteeleUdemy/bigONotation.js
@@ -0,0 +1,5 @@
+ //objectives
+ //describe big O
+ //simpligy big O expressions
+ //define time complexity and space complexity
+ //evaluate the time complexity and space complexity of different algos
