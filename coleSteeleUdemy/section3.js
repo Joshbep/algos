d4abf56e58ff62c3a137cf8182e05d888dff3d6b
jbep <joshuabemartin@gmail.com>
2023-01-13 15:07:45 -0500

    big o of objetcs start

---
+++ coleSteeleUdemy/section3.js
@@ -0,0 +1,23 @@
+//built in data structures for js and its BIG O
+//wokring with Arrays, Objects and built in methods
+
+//understand how objects and arrays work through the lens of BIG O
+//compare and contrast runtimes
+
+//When to use objects
+//when you don't need order
+//when you need fast access/insertion and removal
+
+//Big O of Objects
+//insertion - O(1)
+//Removal - O(1)
+//Searching - O(n)
+//Access - O(1)
+//when you don't need any ordering objects are an excellent choice
+
+
+//methods with objects
+//.keys - O(n)
+//.values - O(n)
+//.entries - O(n)
+//hasOwnProperty - O(1)
