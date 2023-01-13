471c6344d359c1bbbab34e81f34a779745463b2a
jbep <joshuabemartin@gmail.com>
2023-01-13 14:46:30 -0500

    examples of space

--- coleSteeleUdemy/bigONotation.js
+++ coleSteeleUdemy/bigONotation.js
@@ -123,3 +123,14 @@
 //most primitives (booleans, numbers, undefined, null) are constant space
 //strings require O(n) space (where n is the string length)
 //Reference types are generally O(n) where n is the length (for arrays) or the number of keys (for objects)
+
+
+//example
+//O(1)
+//constant space
+
+//remember this is space and time isn't relevant
+
+
+//example
+//O(n)
