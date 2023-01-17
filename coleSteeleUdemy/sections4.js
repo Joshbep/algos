bdf3193dfc584fb9438f6882116229979d1be684
jbep <joshuabemartin@gmail.com>
2023-01-17 12:26:24 -0500

    understanding the problem initial notes

--- coleSteeleUdemy/sections4.js
+++ coleSteeleUdemy/sections4.js
@@ -16,3 +16,11 @@
 //break it down
 //solve/simplify
 //look back and refactor
+
+//UNDERSTANDING PROBLEMS
+//can you restate the problem in my own words?
+//what are the inputs that go into the problem?
+//what are the outputs that should come from the solution to the problem
+//can the outputs be determined from the inputs? in other words
+//do I have enough information to solve the problem?(you may not be able to answer this question until you set about solving the problem. That'sns okay; it's still worth considering the question at this early stage
+//
