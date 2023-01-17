11c772b7559c07ebaa41638a4e709b069688b1b6
jbep <joshuabemartin@gmail.com>
2023-01-17 12:20:42 -0500

    steps to solving algos

---
+++ coleSteeleUdemy/sections4.js
@@ -0,0 +1,18 @@
+//algos and problems solving patterns
+
+//what is an algorithm?
+//a process or a set of steps to accomplish a certain task
+
+//HOW DO YOU IMPROVE
+//1. Devise a plan for solving problems
+//2. Master common problem solving patterns
+
+//A simple, foolproof, magical, miraculous, fail-safe approach
+//* NOT REALLY
+
+//Problem SOLVING
+//Understand the problem
+//explore concrete examples
+//break it down
+//solve/simplify
+//look back and refactor
