0a178f944a7fb38c7cf98b74b5ceea8bb472f8c5
jbep <joshuabemartin@gmail.com>
2023-01-17 12:33:23 -0500

    understanding

--- coleSteeleUdemy/sections4.js
+++ coleSteeleUdemy/sections4.js
@@ -18,9 +18,22 @@
 //look back and refactor
 
 //UNDERSTANDING PROBLEMS
-//can you restate the problem in my own words?
-//what are the inputs that go into the problem?
-//what are the outputs that should come from the solution to the problem
-//can the outputs be determined from the inputs? in other words
+//1. can you restate the problem in my own words?
+//2. what are the inputs that go into the problem?
+//3. what are the outputs that should come from the solution to the problem
+//4. can the outputs be determined from the inputs? in other words
 //do I have enough information to solve the problem?(you may not be able to answer this question until you set about solving the problem. That'sns okay; it's still worth considering the question at this early stage
-//
+//5. how should I label the important pieces of data that are a apart of this problem?
+
+//write a function which takes two numbers and returns their sum
+
+//Understanding the problem
+//1.implement addition
+//2.
+// - ints?
+// - floats?
+// - what about string for large numbers
+//3. 
+//-int? float? string?
+//4.
+//5.
