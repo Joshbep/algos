b8cdff2e7531db3d6909b1958575d72077960fab
jbep <joshuabemartin@gmail.com>
2023-01-18 13:57:20 -0500

    refactored again

--- coleSteeleUdemy/sections4.js
+++ coleSteeleUdemy/sections4.js
@@ -111,16 +111,27 @@
 // - can you think of other ways to refactor?
 // - how have other people solved this problem?
 
+// function charCount(str) {
+//   let result = {};
+//   for(let char of str) {
+//     char = char.toLowerCase();
+//     if(/[a-z0-9]/.test(char)) {
+//       if(result[char] > 0) {
+//         result[char]++;
+//       } else {
+//         result[char] = 1;
+//       }
+//     }
+//   }
+//   return result
+// }
+
 function charCount(str) {
   let result = {};
   for(let char of str) {
     char = char.toLowerCase();
     if(/[a-z0-9]/.test(char)) {
-      if(result[char] > 0) {
-        result[char]++;
-      } else {
-        result[char] = 1;
-      }
+      result[char] == ++result[char]  || 1;
     }
   }
   return result
