9b0ed456ab0d0632cb1896302cce0b1a94397ce6
jbep <joshuabemartin@gmail.com>
2023-01-19 12:19:08 -0500

    some patterns

---
+++ coleSteeleUdemy/section5.js
@@ -0,0 +1,15 @@
+//how do you improve?
+//1. devise a plan for solving problems
+//2. master common problem solving patterns
+
+//next up problem solving paterns
+
+//some Patterns
+//Frequency Counter
+//Multiple Pointers
+//sliding window
+//divide and conquer
+//dynamic programming
+//greedy algorithms
+//backtracking
+//many more
