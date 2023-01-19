2fcd409578e977cf69a90870ef563475e28cbb97
jbep <joshuabemartin@gmail.com>
2023-01-19 12:32:42 -0500

    naive solution

--- coleSteeleUdemy/section5.js
+++ coleSteeleUdemy/section5.js
@@ -13,3 +13,25 @@
 //greedy algorithms
 //backtracking
 //many more
+
+//frequency counters
+//this pattern uses objects or sets to collect
+//values/frequencies of values
+//this can often avoid the need for nested loops or O(N^2) operations with arrays/strings
+
+//write a function called same which accepts two arrays. The function should return true if every value in the array has it's corresponding value squared in the second array. The frequency of values must be the same
+
+//a naive solution
+function same(arr1, arr2) {
+  if(arr1.length !== arr2.length) {
+    return false;
+  }
+  for(let i = 0; i < arr1.length; i++) {
+    let correctIdx = arr2.indexOf(arr1[i] ** 2)
+    if(correctIdx === -1) {
+      return false;
+    }
+    arr2.splice(correctIdx,1);
+  }
+  return true;
+}
