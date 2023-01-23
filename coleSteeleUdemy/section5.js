208dd1716d23d6a4a079fb6ff946a4d6e4384205
jbep <joshuabemartin@gmail.com>
2023-01-23 17:19:42 -0500

    divide and conquer pattern video

--- coleSteeleUdemy/section5.js
+++ coleSteeleUdemy/section5.js
@@ -184,18 +184,28 @@
 
 //sliding window
 
-function maxSubArraySum(arr, num) {
-  let maxSum = 0;
-  let tempSum = 0;
-  if(arr.length < num) return null;
-  for(let i = 0; i < num; i++) {
-    maxSum += arr[i]
-  }
-  tempSum = maxSum;
-  for(let i = num; i < arr.length; i++) {
-    tempSum = tempSum - arr[i - num] + arr[i];
-    maxSum = Math.max(maxSum, tempSum);
-  }
-  return maxSum
-}
-console.log(maxSubArraySum([2,6,9,2,1,8,5,6,3, 7, 8], 4))
+// function maxSubArraySum(arr, num) {
+//   let maxSum = 0;
+//   let tempSum = 0;
+//   if(arr.length < num) return null;
+//   for(let i = 0; i < num; i++) {
+//     maxSum += arr[i]
+//   }
+//   tempSum = maxSum;
+//   for(let i = num; i < arr.length; i++) {
+//     tempSum = tempSum - arr[i - num] + arr[i];
+//     maxSum = Math.max(maxSum, tempSum);
+//   }
+//   return maxSum
+// }
+// console.log(maxSubArraySum([2,6,9,2,1,8,5,6,3, 7, 8], 4))
+
+//divide and conquer
+//this pattern involves dividing a data set into smaller chunks and then repeating a process with a subset of data
+//this pattern can tremendously decrease time complexity
+
+//good solutions
+//linear search and binary search
+// function search() {
+//
+// }
