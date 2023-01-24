4d95e45cc8c7f7624b5595f6b85a4fb72106a170
jbep <joshuabemartin@gmail.com>
2023-01-24 13:40:00 -0500

    recursion notes

--- coleSteeleUdemy/section7.js
+++ coleSteeleUdemy/section7.js
@@ -12,3 +12,25 @@
 
 //what is recursion?
 //a process (a function in our case) that calls itself
+
+//in almost all program languaged there is a built in data structure that manages
+//what happens when functions are invoked
+
+//its named the call stack
+
+//The call stack
+//its called a data structure - we'll talk about that later
+//anytime a function is invoked it is placed (pushed) on the top of the call stack
+//when javascript sees the return keyword or when the function ends, the compiler will remove (pop)
+
+// function wakeUp() {
+//   takeShower()
+//   eatBreakfast()
+//   console.log("ok ready to go")
+// }
+//
+// wakeUp();
+
+//why do I care?
+//you are uysed to functions being pushed on the call stack and popped off when they are done
+//when we write recursive functions, we keep pushing new functions onto the call stack
