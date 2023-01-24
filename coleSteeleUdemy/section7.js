8c244874fb372eaee2801a2ec79c0738d53359b2
jbep <joshuabemartin@gmail.com>
2023-01-24 13:32:48 -0500

    recursion notes

--- coleSteeleUdemy/section7.js
+++ coleSteeleUdemy/section7.js
@@ -9,3 +9,6 @@
 //understand two essential components
 //call stack
 //use helper method recursion and pure recursion to solve more difficult problems
+
+//what is recursion?
+//a process (a function in our case) that calls itself
