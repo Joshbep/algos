8f7f842745eae8404ecb1ab387c501527194a77c
jbep <joshuabemartin@gmail.com>
2023-01-24 13:45:47 -0500

    first recurive function

--- coleSteeleUdemy/section7.js
+++ coleSteeleUdemy/section7.js
@@ -34,3 +34,24 @@
 //why do I care?
 //you are uysed to functions being pushed on the call stack and popped off when they are done
 //when we write recursive functions, we keep pushing new functions onto the call stack
+
+
+//base case
+//the condition when the recursion ends
+//this is the most important concept to understand
+
+
+//two essential parts of a recursive function
+//base case
+//different input
+
+//first recursive function
+
+function countDown(num) {
+  if(num <=0 ){
+    console.log("all done!");
+    return;
+  }
+  num--;
+  countDown(num);
+}
