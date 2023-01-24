aa15eaf4ee4a789e852e9ca38098673fb6b7d729
jbep <joshuabemartin@gmail.com>
2023-01-24 13:58:16 -0500

    third recurive function

--- coleSteeleUdemy/section7.js
+++ coleSteeleUdemy/section7.js
@@ -62,11 +62,29 @@
 
 //second recursive function
 
-function sumRange(num) {
-  if(num === 1) return 1;
-  return num + sumRange(num-1)
-}
-
+// function sumRange(num) {
+//   if(num === 1) return 1;
+//   return num + sumRange(num-1)
+// }
+//
+// console.log(sumRange(3))
 //can you spot base case?
 //do you notice the different input?
 //what would happen if we didn't return
+
+//factorial
+//
+// function factorial(num) {
+//   let total = 1
+//   for(let i = num; i > 1; i--) {
+//     total *=i
+//   }
+//   return total
+// }
+// console.log(factorial(3))
+
+function factorial(num) {
+  if(num === 1) return 1
+  return num * factorial(num - 1)
+}
+console.log(factorial(4))
