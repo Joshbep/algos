a395a738c65b3e0b801897f63f3648dd2fcef454
jbep <joshuabemartin@gmail.com>
2023-01-25 11:55:56 -0500

    factorial recursive solution

--- coleSteeleUdemy/section8.js
+++ coleSteeleUdemy/section8.js
@@ -22,3 +22,16 @@
 // 3 * (power(3, 3 - 1))
 // 3 *  9  = 27
 // console.log(power(3,3))
+
+
+//understand problem
+//I want to return the factorial of num which is a paramter passed in
+// if num is 4, funciton should perform 4 * 3 * 2 * 1
+//return base case if factorial is 0
+//return factorial solution
+// function factorial(num) {
+//   if(num === 0) return 1;
+//   return num * factorial(num - 1)
+// }
+//
+// console.log(factorial(4))
