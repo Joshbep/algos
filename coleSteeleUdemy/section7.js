9aff9782bec086e5227663e6089b28ed9ff1d5f0
jbep <joshuabemartin@gmail.com>
2023-01-25 11:38:57 -0500

    finished section7

--- coleSteeleUdemy/section7.js
+++ coleSteeleUdemy/section7.js
@@ -99,38 +99,40 @@
 //calls an inner function which is recursive
 
 
-// function collectOdds(nums) {
-//   let result = []
-//
-//   function helper(helperInput) {
-//     if(helperInput.length === 0) {
-//       return
-//     }
-//
-//     if(helperInput[0] % 2 !== 0){
-//       result.push(helperInput[0])
-//     }
-//
-//     helper(helperInput.slice(1))
-//   }
-//   helper(arr)
-//   return result;
-// }
+function collectOdds(arr) {
+  let result = []
+
+  function helper(helperInput) {
+    if(helperInput.length === 0) {
+      return
+    }
+
+    if(helperInput[0] % 2 !== 0){
+      result.push(helperInput[0])
+    }
+
+    helper(helperInput.slice(1))
+  }
+  helper(arr)
+  return result;
+}
 
 
 //Pure Recursion
 
-function collectOddValues(arr){
-  let newArr = [];
+// function collectOddValues(arr){
+//   let newArr = [];
+//
+//   if(arr.length === 0) {
+//     return newArr;
+//   }
+//
+//   if(arr[0] % 2 !== 0) {
+//     newArr.push(arr[0]);
+//   }
+//
+//   newArr = newArr.concat(collectOddValues(arr.slice(1)))
+//   return newArr;
+// }
 
-  if(arr.length === 0) {
-    return newArr;
-  }
-
-  if(arr[0] % 2 !== 0) {
-    newArr.push(arr[0]);
-  }
-
-  newArr = newArr.concat(collectOddValues(arr.slice(1)))
-  return newArr;
-}
+console.log(collectOdds([1,2,3,4,5]))
