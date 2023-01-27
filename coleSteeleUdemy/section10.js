860646cf96f9b8d6bb6db0464cb4520c50abcb8c
jbep <joshuabemartin@gmail.com>
2023-01-27 12:27:20 -0500

    BIG O for binary search

--- coleSteeleUdemy/section10.js
+++ coleSteeleUdemy/section10.js
@@ -62,24 +62,34 @@
 //if you never find the value return -1
 
 
+//
+// function binarySearch(arr, elem) {
+//   let start = 0;
+//   let end = arr.length - 1;
+//   let middle = Math.floor((start + end) / 2);
+//   while(arr[middle] !== elem && start <= end) {
+//     if(elem < arr[middle]) {
+//       end = middle - 1;
+//     } else {
+//       start = middle + 1;
+//     }
+//     middle = Math.floor((start + end) / 2);
+//   }
+//   if(arr[middle] === elem) {
+//     return middle
+//   } else {
+//     return -1;
+//   }
+// }
+//
+// console.log(binarySearch([2,5,6,9,13,15,28], 5))
 
-function binarySearch(arr, elem) {
-  let start = 0;
-  let end = arr.length - 1;
-  let middle = Math.floor((start + end) / 2);
-  while(arr[middle] !== elem && start <= end) {
-    if(elem < arr[middle]) {
-      end = middle - 1;
-    } else {
-      start = middle + 1;
-    }
-    middle = Math.floor((start + end) / 2);
-  }
-  if(arr[middle] === elem) {
-    return middle
-  } else {
-    return -1;
-  }
-}
 
-console.log(binarySearch([2,5,6,9,13,15,28]))
+//BINARY SEARCH BIG O
+//what about BIG O
+
+//WORST AND AVERAGE
+//O(log n)
+
+//BEST CASE
+//O(1)
