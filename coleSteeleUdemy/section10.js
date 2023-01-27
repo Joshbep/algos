f4afb4d698dcf4e6514dfb3b5f00ec4e45a38bcf
jbep <joshuabemartin@gmail.com>
2023-01-27 12:16:28 -0500

    binary search algo

--- coleSteeleUdemy/section10.js
+++ coleSteeleUdemy/section10.js
@@ -60,3 +60,26 @@
   //if the value is too small, move the left pointer up
   //if the value is too large move the right pointer down
 //if you never find the value return -1
+
+
+
+function binarySearch(arr, elem) {
+  let start = 0;
+  let end = arr.length - 1;
+  let middle = Math.floor((start + end) / 2);
+  while(arr[middle] !== elem && start <= end) {
+    if(elem < arr[middle]) {
+      end = middle - 1;
+    } else {
+      start = middle + 1;
+    }
+    middle = Math.floor((start + end) / 2);
+  }
+  if(arr[middle] === elem) {
+    return middle
+  } else {
+    return -1;
+  }
+}
+
+console.log(binarySearch([2,5,6,9,13,15,28]))
