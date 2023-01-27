33ade9ca6937dbf0499c416832f70250814afd04
jbep <joshuabemartin@gmail.com>
2023-01-27 11:28:25 -0500

    start binary search funtion

--- coleSteeleUdemy/section10.js
+++ coleSteeleUdemy/section10.js
@@ -49,3 +49,14 @@
 
 //DIVIDE AND CONQUER
 //Split in half and if bigger move up
+
+
+//Binary Search Pseudocode
+//This function accepts a sorted array and a value
+//create a left pointer at the start of the array, and a right pointer at the end of the array
+//while the left pointer comes before the right pointer:
+  // create a pointer in the middle
+  //if you find the value you want, return the index
+  //if the value is too small, move the left pointer up
+  //if the value is too large move the right pointer down
+//if you never find the value return -1
