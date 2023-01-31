84230a7a4fe1d59f169566a26eb7ab3e286df157
jbep <joshuabemartin@gmail.com>
2023-01-31 12:22:24 -0500

    big o complexity

--- coleSteeleUdemy/section11.js
+++ coleSteeleUdemy/section11.js
@@ -73,19 +73,25 @@
 
 //bubble sort optimization
 
-function bubbleSort(arr) {
-  let noSwaps;
-  for(let i = arr.length; i > 0; i--) {
-    noSwaps = true;
-    for (let j = 0; j < i - 1; j++) {
-      if(arr[j] > arr[j + 1]) {
-        let temp = arr[j]
-        arr[j] = arr[j + 1]
-        arr[j + 1] = temp
-        noSwaps = false;
-      }
-    }
-    if(noSwaps) break;
-  }
-  return arr;
-}
+// function bubbleSort(arr) {
+//   let noSwaps;
+//   for(let i = arr.length; i > 0; i--) {
+//     noSwaps = true;
+//     for (let j = 0; j < i - 1; j++) {
+//       if(arr[j] > arr[j + 1]) {
+//         let temp = arr[j]
+//         arr[j] = arr[j + 1]
+//         arr[j + 1] = temp
+//         noSwaps = false;
+//       }
+//     }
+//     if(noSwaps) break;
+//   }
+//   return arr;
+// }
+
+//BIG O
+//best case
+// time O(n)
+//Average
+// O(n^2)
