08b86be2e6f83087c520a8919de9a193a7e11edf
jbep <joshuabemartin@gmail.com>
2023-01-31 12:51:21 -0500

    selection sort time complexity

--- coleSteeleUdemy/section12.js
+++ coleSteeleUdemy/section12.js
@@ -2,19 +2,23 @@
 
 //similar to bubble sort. but instead of first placing large values into sorted position, it places small values into sorted position
 
-function selectionSort(arr) {
-  for(let i = 0; i < arr.length; i++) {
-    let min = i;
-    for(let j = i + 1; j < arr.length; j++) {
-      if(arr[j] < arr[min]) {
-        min = j;
-      }
-    }
-    if(i !== min) {
-      let temp = arr[i];
-      arr[i] = arr[min];
-      arr[min] = temp;
-    }
-  }
-  return arr;
-}
+// function selectionSort(arr) {
+//   for(let i = 0; i < arr.length; i++) {
+//     let min = i;
+//     for(let j = i + 1; j < arr.length; j++) {
+//       if(arr[j] < arr[min]) {
+//         min = j;
+//       }
+//     }
+//     if(i !== min) {
+//       let temp = arr[i];
+//       arr[i] = arr[min];
+//       arr[min] = temp;
+//     }
+//   }
+//   return arr;
+// }
+
+//selection sort BIG O
+//time
+//average O(n^2)
