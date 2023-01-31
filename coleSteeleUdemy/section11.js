e5f2e8600f9941f177b7fdc5b7a195e47a524eaf
jbep <joshuabemartin@gmail.com>
2023-01-31 11:48:51 -0500

    sorting intro

---
+++ coleSteeleUdemy/section11.js
@@ -0,0 +1,17 @@
+//what is sorting?
+//sorting is the process of rearranging the items in a collection (e.g. an array) so that the items are in some kind of order
+//examples
+//sorting numbers from smallest to largest
+//sorting names alphabeticaly
+//sorting movies based on release year
+//sorting movies based on revenue
+
+// function sort(arr) {
+//   return arr
+// }
+// sort([23,45,6,12,13])
+//should return 6,12,13,23,45
+
+//why do we need to learn this?
+//sorting is an incredibly common task, so it's good to know how it works
+//there are many ways to sort things, and different techniques have their own advantages and disadvanatges
