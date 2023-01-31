cf13d7e1bdb924bdb9d89141355666a806aaab9d
jbep <joshuabemartin@gmail.com>
2023-01-31 12:16:37 -0500

    simple bubble sort implementation

--- coleSteeleUdemy/section11.js
+++ coleSteeleUdemy/section11.js
@@ -41,3 +41,32 @@
 
 //BUBBLE SORT
 // a sorting algo where the largest values bubble up to the top
+
+//Before we sort, we must swap!
+//many sorting algos involve some type of swapping functionality (e.g. swapping to numbers to put them in order)
+
+// function swap(arr, idx1, idx2) {
+//   let temp = arr[idx1];
+//   arr[idx1] = arr[idx2];
+//   arr[idx2] = temp;
+// }
+//
+// const swap = (arr, idx1, idx2) => {
+//   [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+// }
+
+// function bubbleSort(arr) {
+//   for(let i = arr.length; i > 0; i--) {
+//     for (let j = 0; j < i - 1; j++) {
+//       if(arr[j] > arr[j + 1]) {
+//         let temp = arr[j]
+//         arr[j] = arr[j + 1]
+//         arr[j + 1] = temp
+//       }
+//     }
+//   }
+//   return arr;
+// }
+// console.log(bubbleSort([37, 45, 29, 8]))
+
+//bubble sort bubbles the largest to the top
