c3b6def7aa6aae80cdf688141d60fd57ddbdd714
jbep <joshuabemartin@gmail.com>
2023-02-01 11:50:34 -0500

    insertion sort

--- coleSteeleUdemy/section13.js
+++ coleSteeleUdemy/section13.js
@@ -6,3 +6,14 @@
 //now compare the second element with the one before it and swap if necessary
 //continue to the next element and if it is in th eincorrect order, iterate through the sorted portion
 //(i.e. the left side) to place the element in the correct place.
+
+function insetionSort(arr) {
+  for(let i = 1; i < arr.length; i++) {
+    let currentVal = arr[i];
+    for(let j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+      arr[j + 1] = arr[j]
+    }
+    arr[j + 1] = currentVal
+  }
+  return arr;
+}
