0828435e7db161d240b31e5c34bfb6309123e9e6
jbep <joshuabemartin@gmail.com>
2023-02-01 12:05:24 -0500

    big O for sorting algos

---
+++ coleSteeleUdemy/section14.js
@@ -0,0 +1,5 @@
+//Big O of sorting algos
+          //best    //average   //worst   /space
+//bubble    O(n)     O(n^2)      O(n^2)     O(1)
+//insertion O(n)     O(n^2)      O(n^2)     O(1)
+//selection O(n^2)   O(n^2)      O(n^2)     O(1)
