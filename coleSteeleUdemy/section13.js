51dad8155e2e8690195aab6a65d2e6b807b98ca0
jbep <joshuabemartin@gmail.com>
2023-02-01 11:41:41 -0500

    insertion sort intro

---
+++ coleSteeleUdemy/section13.js
@@ -0,0 +1,8 @@
+//insertion sort intro
+//builds up the sort by gradually creating a large left half which is always sorted
+
+//pseudocode
+//start by picking the second element in the array
+//now compare the second element with the one before it and swap if necessary
+//continue to the next element and if it is in th eincorrect order, iterate through the sorted portion
+//(i.e. the left side) to place the element in the correct place.
