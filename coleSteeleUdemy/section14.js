107342e5654d97244c5ab4ae0fc193fb7708bd9a
jbep <joshuabemartin@gmail.com>
2023-02-01 12:07:47 -0500

    finished section 14

--- coleSteeleUdemy/section14.js
+++ coleSteeleUdemy/section14.js
@@ -3,3 +3,8 @@
 //bubble    O(n)     O(n^2)      O(n^2)     O(1)
 //insertion O(n)     O(n^2)      O(n^2)     O(1)
 //selection O(n^2)   O(n^2)      O(n^2)     O(1)
+
+//sorting is fundamental!
+//bubble sort, selection sort, and insertion sort are all roughly equivalent
+//all have average time complexities that are quadratic
+//we can do better... but we need more complex algos
