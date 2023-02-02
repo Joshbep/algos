a1d28402f21046eda755f7a3bb0e150f80141e0f
jbep <joshuabemartin@gmail.com>
2023-02-02 07:37:57 -0500

    merge sort function implemented

--- coleSteeleUdemy/section15.js
+++ coleSteeleUdemy/section15.js
@@ -57,9 +57,21 @@ function merge(arr1, arr2) {
   return results;
 }
 
-console.log(merge([1,10,50], [2,14,99,100]))
+// console.log(merge([1,10,50], [2,14,99,100]))
 
 //sorting part of merge sort
 //break up the array into halves until you have arrays that are empty or have one element
 //once you have smaller sorted arrays, merge those arrays with other sorted arrays until you are back at the full length of the array
 //once the array has beem ,erged back together, return the merged (and sorted array)
+
+//divide and conquer
+//always left,right mid
+function mergeSort(arr) {
+  if(arr.length <= 1) return arr;
+  let mid = Math.floor(arr.length/2);
+  let left = mergeSort(arr.slice(0,mid));
+  let right = mergeSort(arr.slice(mid));
+  return merge(left, right)
+}
+
+console.log(mergeSort([10,24,76,73,72,1,9]))
