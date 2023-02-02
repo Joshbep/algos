cf68588500f03483b1b36c27df9167b3e92e2d52
jbep <joshuabemartin@gmail.com>
2023-02-02 07:05:04 -0500

    merge sort video

---
+++ coleSteeleUdemy/section15.js
@@ -0,0 +1,18 @@
+//intermediate sorting algos
+
+//pseudocode
+
+//objectives
+//understand the limitations of the sorting algos weve learned so far
+//implement merge sort
+//implement quick sort
+//implement radix sort
+
+//the simpler algos weve learned so far don't scale well
+//try out bubbles ort on an array of 10,000 elements, it will take quite some time
+
+//faster sorts
+//they can improve time complexity from O(n^2) to O(n log n)
+//theres a trade off between efficiency and simplicity
+//the more efficient algos are much less simple, and generally
+//take longer to understand
