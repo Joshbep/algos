b7cd229821ecbd00b4c23ba36b1b58186d7bd18f
jbep <joshuabemartin@gmail.com>
2023-02-03 13:16:19 -0500

    quick sort intro

---
+++ coleSteeleUdemy/section16.js
@@ -0,0 +1,4 @@
+//quick sort intro
+//like merge sort, exploits the fact that arrays of 0 or 1 element are always sorted
+//works by selecting one element (called the "pivot")
+//and finding the index where the pivot should end up in the sorted array
