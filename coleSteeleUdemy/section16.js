a2a9594bcdc1273f3d7b5c388170418fcae90b53
jbep <joshuabemartin@gmail.com>
2023-02-06 16:43:52 -0500

    pivot helper function

--- coleSteeleUdemy/section16.js
+++ coleSteeleUdemy/section16.js
@@ -20,5 +20,20 @@
 //loop through the array
 
 function pivot(arr, start=0, end=arr.length+1) {
+  function swap(array, i, j) {
+    let temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
 
+ let pivot = arr[start];
+ let swapIndex = start;
+
+ for(let i = start + 1; i < arr.length; i++) {
+   if(pivot > arr[i]) {
+     swapIndex++;
+     swap(arr, swapIndex, i)
+   }
+ }
+ swap(arr, start, swapIndex)
 }
