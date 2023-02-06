50b2cfc1f005641026789c9b2835529ec4929ece
jbep <joshuabemartin@gmail.com>
2023-02-06 16:04:02 -0500

    section 16 notes

--- coleSteeleUdemy/section16.js
+++ coleSteeleUdemy/section16.js
@@ -2,3 +2,19 @@
 //like merge sort, exploits the fact that arrays of 0 or 1 element are always sorted
 //works by selecting one element (called the "pivot")
 //and finding the index where the pivot should end up in the sorted array
+
+
+//Quick Start Function
+//PIVOT HELPER
+
+//Picking a pivot
+//the runtime of quick sort depends in part on how one selects the pivot
+//ideally, the pivot shoudl be chosen so that it's roughly the media value in the data set you're sorting
+//for simplicity we'll always choose the pivot to be the first eleement
+
+
+// pivot pseudocode
+//it will help to accept three arguments: an array, a starting index, and an end index
+//grab pivot
+//store current pivot inde
+//loop through the array
