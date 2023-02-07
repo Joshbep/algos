8b41fa138d22f979558479f4f397ec0608f0b86b
jbep <joshuabemartin@gmail.com>
2023-02-07 14:41:49 -0500

    big o of quick sorT

--- coleSteeleUdemy/section16.js
+++ coleSteeleUdemy/section16.js
@@ -19,38 +19,46 @@
 //store current pivot inde
 //loop through the array
 
-function pivot(arr, start=0, end=arr.length+1) {
-  function swap(array, i, j) {
-    let temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-  }
+// function pivot(arr, start=0, end=arr.length+1) {
+//   function swap(array, i, j) {
+//     let temp = array[i];
+//     array[i] = array[j];
+//     array[j] = temp;
+//   }
+//
+//  let pivot = arr[start];
+//  let swapIndex = start;
+//
+//  for(let i = start + 1; i < arr.length; i++) {
+//    if(pivot > arr[i]) {
+//      swapIndex++;
+//      swap(arr, swapIndex, i)
+//    }
+//  }
+//  swap(arr, start, swapIndex);
+//  return swapIndex;
+// }
+//
+// //quick sort pseudocode
+// //call the pivot helper
+// //when the helper returns to you the updated pivot index, recursively call the pivot helper on the subarray
+// //to the left of that index, and the subarray to the reight of the index
+//
+// function quickSort(arr, left=0, right=arr.length-1) {
+//   if(left < right) {
+//     let pivotIndex = pivot(arr, left, right);
+//     //left
+//     quickSort(arr,left,pivotIndex-1);
+//     //right
+//     quickSort(arr, pivotIndex+1,right);
+//   }
+//   return arr;
+// }
 
- let pivot = arr[start];
- let swapIndex = start;
 
- for(let i = start + 1; i < arr.length; i++) {
-   if(pivot > arr[i]) {
-     swapIndex++;
-     swap(arr, swapIndex, i)
-   }
- }
- swap(arr, start, swapIndex);
- return swapIndex;
-}
+//Big O of quick Sort
 
-//quick sort pseudocode
-//call the pivot helper
-//when the helper returns to you the updated pivot index, recursively call the pivot helper on the subarray
-//to the left of that index, and the subarray to the reight of the index
-
-function quickSort(arr, left=0, right=arr.length-1) {
-  if(left < right) {
-    let pivotIndex = pivot(arr, left, right);
-    //left
-    quickSort(arr,left,pivotIndex-1);
-    //right
-    quickSort(arr, pivotIndex+1,right);
-  }
-  return arr;
-}
+//time best O(n log n)
+// time average O(n log n)
+// time worst O(n^2)
+//Space )(log n)
