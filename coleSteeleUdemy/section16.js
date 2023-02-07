e2c511406ed7cd3ca903ef9b6a8e6fd068358998
jbep <joshuabemartin@gmail.com>
2023-02-07 14:33:38 -0500

    quick sort implementation

--- coleSteeleUdemy/section16.js
+++ coleSteeleUdemy/section16.js
@@ -42,3 +42,14 @@ function pivot(arr, start=0, end=arr.length+1) {
 //call the pivot helper
 //when the helper returns to you the updated pivot index, recursively call the pivot helper on the subarray
 //to the left of that index, and the subarray to the reight of the index
+
+function quickSort(arr, left=0, right=arr.length-1) {
+  if(left < right) {
+    let pivotIndex = pivot(arr, left, right);
+    //left
+    quickSort(arr,left,pivotIndex-1);
+    //right
+    quickSort(arr, pivotIndex+1,right);
+  }
+  return arr;
+}
