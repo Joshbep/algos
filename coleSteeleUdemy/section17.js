f96e54a4b5f6ce854d626897116bca0d1943f069
jbep <joshuabemartin@gmail.com>
2023-02-08 13:14:18 -0500

    radix sort helpers

--- coleSteeleUdemy/section17.js
+++ coleSteeleUdemy/section17.js
@@ -19,3 +19,19 @@
 //more digits means a bigger number
 
 //how does it work?
+
+
+//radix sort works with special cases
+
+///radix sort helpers
+//in order to implement radix sort, it's helpful to build a few helper functions first:
+// getDigit(num, place) - returns the digit in num at the given place value
+
+function getDigit(num, i) {
+  return Math.floor(Math.abs(num) / Math.pow(10, i) % 10)
+}
+
+function digitCount(num) {
+  if (num === 0) return 1;
+  return Math.floor(Math.log10(Math.abs(num))) + 1
+}
