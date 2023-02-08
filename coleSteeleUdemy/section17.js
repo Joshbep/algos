999d2620548f908471f2cbb4120406ad9d4039d1
jbep <joshuabemartin@gmail.com>
2023-02-08 13:44:35 -0500

    radix sort implementation

--- coleSteeleUdemy/section17.js
+++ coleSteeleUdemy/section17.js
@@ -42,3 +42,25 @@ function mostDigits(nums) {
     maxDigits = Math.max(maxDigits, digitCount(nums[i]));
   }
 }
+
+//radix sort psuedocode
+//define a function that accepts list of numbers
+// figure out how many digits the largest number has
+//loop from k=0 up to this largest number of digits
+//for each iteration of the loop:
+//-create buckets for each digit (0 to 9)
+//-place each number in the corresponding bucket based on its kth digit
+//replace our existing array with values in our buckets starting qith 0 going up to 9
+
+function radixSort(nums) {
+  let maxDigitCount = mostDigits(nums)
+  for(let k = 0; k < maxDigitCount; k++) {
+    let digitBuckets = Array.from({length: 10}, () => [])
+    for(let i = 0; i < nums.length; i++){
+      let digit = getDigit(nums[i],k)
+      digitBuckets[digit].push(nums[i]);
+    }
+    nums = [].concat(...digitBuckets);
+  }
+  return nums
+}
