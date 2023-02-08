a4087202d0fb0b6b62a949b1dac2053451436a06
jbep <joshuabemartin@gmail.com>
2023-02-08 12:54:16 -0500

    section 17 notes

--- coleSteeleUdemy/section17.js
+++ coleSteeleUdemy/section17.js
@@ -1,1 +1,13 @@
 //radix sort intro
+
+//comparison sorts
+//average time complexity
+//bubble sort O(n^2)
+//insertion sort - O(n^2)
+//selection sort (O(n^2))
+
+//quick sort - O(n log n)
+//merge sort - O(n log n)
+
+//can we do better?
+//yes, but not by making comparisons
