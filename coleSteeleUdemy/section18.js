adcb7dacb8076674c9e12a1fa9842ba69631a465
jbep <joshuabemartin@gmail.com>
2023-02-09 14:49:45 -0500

    adding instance methods

--- coleSteeleUdemy/section18.js
+++ coleSteeleUdemy/section18.js
@@ -30,3 +30,43 @@
 //why do we need to learn this?
 
 //we're going to implement data structures as classes!
+
+// the syntax
+
+class Student {
+  constructor(firstName, lastName, year) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.year = year;
+    this.tardies = 0;
+    this.scores=[]
+  }
+  fullName() {
+    return `Your full name is ${this.firstName} ${this.lastName}`;
+  }
+  markLate(){
+    this.tardies+=1;
+    return `${this.firstName} ${this.lastName} has been late ${this.tardies}`;
+  }
+  addScore(score){
+    this.scores.push(score);
+    return this.scores
+  }
+  calculateAverage(){
+    let sum = this.scores.reduce(function(a,b){return a+b;})
+    return sum/this.scores.length;
+  }
+}
+
+//the method to create a new object must be called constructor
+//the class keyword creates a constant, so you can not redefine it.
+//watch out for the syntax as well.
+
+//we use the new keyword
+let firstStudent = new Student("Colt", "Steele", 4);
+let secondStudent = new Student("Blue", "Steele", 2)
+let kitty = new Student("kitty", "cat", 1)
+
+//instance methods
+firstStudent.fullName();
+firstStudent.markLate();
