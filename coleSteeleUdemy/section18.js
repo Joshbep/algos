4e73dc4249731082e5508a9984999ac532ba238a
jbep <joshuabemartin@gmail.com>
2023-02-09 14:26:11 -0500

    why leanrs es2015 classes

--- coleSteeleUdemy/section18.js
+++ coleSteeleUdemy/section18.js
@@ -11,3 +11,22 @@
 //learn doubly linked lists
 
 //pay attention to the prereqs for each section
+
+//ES2015
+//class syntax
+
+//objectives
+//explain what a class is
+//understand how javascript implements the idea od classes
+//define terms like abstraction, encapsulation and polymorphism
+//use es2015 classes ti implement data structures
+
+//what is a class?
+//a blueprint for creating objects with pre-defined properties and methods
+
+//does javascript really have them?
+//ehh.... not really
+
+//why do we need to learn this?
+
+//we're going to implement data structures as classes!
