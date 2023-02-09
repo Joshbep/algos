f83f9cf4a410f0b91c3fe2f2d677a5ff587cfeea
jbep <joshuabemartin@gmail.com>
2023-02-09 13:58:37 -0500

    intro to data structures

--- coleSteeleUdemy/section18.js
+++ coleSteeleUdemy/section18.js
@@ -1,1 +1,7 @@
 //intro to data structures
+
+//data structures are collections of values, the rekationships among them, and the functions or operations that can be applied to the data.
+
+//why so many?
+//different data structures excel at different things.
+//some are highly specialized, while others (like arrays) are more generally used
