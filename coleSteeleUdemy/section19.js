11328c4fa9d1bab669735c80173d3ea68b94aaa9
jbep <joshuabemartin@gmail.com>
2023-02-10 12:08:32 -0500

    pushing linked lists

--- coleSteeleUdemy/section19.js
+++ coleSteeleUdemy/section19.js
@@ -24,3 +24,42 @@
 //indexed in order
 //insertion and deltion can be expensive
 //can quickly be accessed at a specific index
+
+class Node{
+  constructor(val){
+    this.val = val;
+    this.next = null
+  }
+}
+
+class SinglyLinkedList{
+  constructor(){
+    this.lenth = 0;
+    this.head = null;
+    this.tail = null;
+  }
+  push(val){
+
+  }
+}
+
+// let first = new Node("Hi")
+// first.next = new Node("there")
+// first.next.next = new Node("how")
+// first.next.next.next = new Node("are")
+// first.next.next.next.next = new Node("you")
+//
+// list.push("you")
+
+let list = new SignlyLinkedList()
+list.push("hello")
+list.push("goodbye")
+
+
+//pushing
+//adding a new node to the end of the linked list
+
+//pushing pseudocode
+//this function should accept a value
+//create a new node using the value passed to the function
+//if there is no head property on the list, set the head and tail ton be the newly created node
