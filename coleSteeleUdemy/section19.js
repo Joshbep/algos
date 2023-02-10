8d860cc6e8a94bafee7417fe0e097de384742f46
jbep <joshuabemartin@gmail.com>
2023-02-10 11:52:05 -0500

    starting with linked lists

--- coleSteeleUdemy/section19.js
+++ coleSteeleUdemy/section19.js
@@ -4,3 +4,23 @@
 //define what a singly linked list is
 //compare and contrast linked lists with arrays
 //implement insertion, removal and traversal methods on singly linked lists
+
+
+//what is a linked list?
+
+//a data structure that contains a head, tail and length property.
+//linked lists consist of nodes, and each node has a value and a pointer to another node or null
+//singly linked lists only goes one way
+
+
+//comparisons with arrays
+
+//lists
+//do not have indexes
+//connected vio nodes with a next pointer
+//random access is not allowed
+
+//arrays
+//indexed in order
+//insertion and deltion can be expensive
+//can quickly be accessed at a specific index
