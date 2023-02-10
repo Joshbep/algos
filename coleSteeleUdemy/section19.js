92f67f9cad8a60740db8662202a9ee75a18007c9
jbep <joshuabemartin@gmail.com>
2023-02-10 11:46:19 -0500

    starting with linked lists

---
+++ coleSteeleUdemy/section19.js
@@ -0,0 +1,6 @@
+//singly linked lists
+
+//objectives
+//define what a singly linked list is
+//compare and contrast linked lists with arrays
+//implement insertion, removal and traversal methods on singly linked lists
