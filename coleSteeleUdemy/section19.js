c4f2a6f9cda063d562f7d661d3be731556ffc348
jbep <joshuabemartin@gmail.com>
2023-02-15 16:29:35 -0500

    remove method solution

--- coleSteeleUdemy/section19.js
+++ coleSteeleUdemy/section19.js
@@ -126,6 +126,16 @@ class SinglyLinkedList{
     this.length++;
     return true;
   }
+  remove(index){
+    if( index < 0 || index >= length) return null;
+    if(index === 0) return this.shift();
+    if(index === this.length - 1) return this.pop();
+    let prevNode = this.get(index - 1);
+    let removed = prevNode.next;
+    prevNode.next = removed.next;
+    this.length--;
+    return removed;
+  }
 }
 
 // let first = new Node("Hi")
@@ -234,3 +244,23 @@ list.push("hello")
 
 //increment the length
 //return true
+
+
+//Remove
+//remove a node from the linked list at a specific postiion
+
+//remove pseudocode
+//if the index is less than zero or greater than the lenght,
+//return undefined
+
+//if the index is the same as the length-1, pop
+
+//if the index is 0, shift
+
+//otherwise, using the get method, access the node at the index -1
+
+//set the next property on that node to be the next node
+
+//decrement the length
+
+//remove solution above ^^
