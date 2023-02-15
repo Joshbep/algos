ac69f454617d0154d2324e930908438141b4e6f5
jbep <joshuabemartin@gmail.com>
2023-02-15 16:00:14 -0500

    insert solution

--- coleSteeleUdemy/section19.js
+++ coleSteeleUdemy/section19.js
@@ -114,6 +114,18 @@ class SinglyLinkedList{
     }
     return false;
   }
+  insert(index, val) {
+    if(index < 0 || index > this.length) return false;
+    if(index === this.length) return !!this.push(val);
+    if(index === 0) return !!this.unshift(val);
+    let newNode = new Node(val);
+    let prev = this.get(index - 1);
+    let temp = prev.next;
+    prev.next = newNode;
+    newNode.next = temp;
+    this.length++;
+    return true;
+  }
 }
 
 // let first = new Node("Hi")
@@ -220,4 +232,5 @@ list.push("hello")
 //set the next propoerty on that node to be the new node
 //set the next property on the new node to be the previous next
 
-//increment the length 
+//increment the length
+//return true
