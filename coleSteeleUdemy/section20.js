1a49cb4cd0f4f104424b0f3e87ce88b0cf9fc366
jbep <joshuabemartin@gmail.com>
2023-02-20 17:57:19 -0500

    push doubly linked list psuedocode

--- coleSteeleUdemy/section20.js
+++ coleSteeleUdemy/section20.js
@@ -42,6 +42,19 @@ class DoublyLinkedList {
     this.length = 0;
   }
   push() {
-    
+
   }
 }
+
+
+//pushing psuedocode
+//create a new node witht he value passed to the function
+
+//if the head property is null set the head and tail
+//to be the newly created node
+
+//if not, set the next property on the tail to be that node
+
+//set the previous property on the newly created node to be the tail
+
+//set the tail to be the newly created node
