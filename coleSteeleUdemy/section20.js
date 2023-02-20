8a8f672545d0c9a6fb16433c09025e4a0a340fa7
jbep <joshuabemartin@gmail.com>
2023-02-20 17:34:20 -0500

    comparisons for singly and doubly linked lists

--- coleSteeleUdemy/section20.js
+++ coleSteeleUdemy/section20.js
@@ -6,3 +6,13 @@
 //implement basic operations on a dobuly linked list
 
 //almost identical to singly linked lists, except every node has another pointer, to the previous node
+
+//you can not remove tail in singly linked list
+//you can remove tail in doubly linked lists because a doubly linked list goes both ways
+
+//code may be more complicated, bc we are keeping up with two pointers
+//it takes up more memory
+
+//comparisons with singly linked lists
+//more memory === more flexibility
+//it's almost always a tradeoff
