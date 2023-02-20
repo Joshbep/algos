d6ff802e10dcbe87e84df9910037240dfd86d2d8
jbep <joshuabemartin@gmail.com>
2023-02-20 17:22:45 -0500

    doubly linked list lesson objectives

--- coleSteeleUdemy/section20.js
+++ coleSteeleUdemy/section20.js
@@ -1,1 +1,8 @@
-//jj
+//Start of Doubly Linked Lists
+
+//Objectives
+//contruct a doubly linked list
+//compare and contrast doubly and singly linked lists
+//implement basic operations on a dobuly linked list
+
+//almost identical to singly linked lists, except every node has another pointer, to the previous node
