20d2b7a31add628cac9eb36062b927e4ddac6161
jbep <joshuabemartin@gmail.com>
2023-02-28 13:12:35 -0500

    queue implementation

--- coleSteeleUdemy/section21.js
+++ coleSteeleUdemy/section21.js
@@ -132,7 +132,7 @@
 
 //BUILDING A QUEUE WITH AN ARRAY
 
-let q = [];
+// let q = [];
 
 // q.push("first")
 // q.push("second")
@@ -142,9 +142,47 @@ let q = [];
 // //removing from beginging makes this a queue
 // console.log(q)
 
-q.unshift("first")
-q.unshift("second")
-q.unshift("third")
-console.log(q)
-q.pop()
-console.log(q)
+// q.unshift("first")
+// q.unshift("second")
+// q.unshift("third")
+// console.log(q)
+// q.pop()
+// console.log(q)
+
+//building a QUEUE class
+
+class Node{
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor(){
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  enqueue(val){
+    let newNode = new Node(val);
+    if(this.size === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    return ++this.size;
+  }
+  dequeue(){
+    if(!this.first) return null;
+    let temp = this.first;
+    if(this.first === this.last){
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.val;
+  }
+}
