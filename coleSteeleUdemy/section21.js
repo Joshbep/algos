fdf356b00ec66f796036a90a9067a14b6e18484f
jbep <joshuabemartin@gmail.com>
2023-02-28 12:05:04 -0500

    start of stacks

--- coleSteeleUdemy/section21.js
+++ coleSteeleUdemy/section21.js
@@ -1,1 +1,27 @@
 //STACKS
+//objectives
+
+//define what a stack is
+//understand use cases for a stack
+//implement operations on a stack data structure
+
+//what is a stack?
+
+// A LIFO data strcuture
+
+//the last element added to the stack will be the first element
+//removed from the STACK
+
+//think about a stack of plates, or a stack of markers, or stack of anything
+//as you pile it up the last thing(or the topmost thing) is what gets removed first
+
+//weve seen this before
+//the call stack
+
+//where stacks are used
+
+//managing function invocations
+//Undo/Redo
+//routing (the history object) is treated like a stack!
+
+//there is more than one way of implementing a stack
