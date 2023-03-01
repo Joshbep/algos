333c2f17bf323be1522e5705d6372a7abf67c8b7
jbep <joshuabemartin@gmail.com>
2023-03-01 17:09:23 -0500

    intro to trees

--- coleSteeleUdemy/section22.js
+++ coleSteeleUdemy/section22.js
@@ -1,1 +1,35 @@
 //Binary search trees
+
+//objectives
+//define what a tree is
+
+//compare and contrast trees and lists
+
+//explain the differences between trees, binary trees, and binary search trees
+
+//implement operations on BST (binary search trees)
+
+//WHAT IS A TREE
+
+//a data structure that consists of nodes ina  parent/child relationship
+
+
+//lists - linear
+
+//tree - nonlinear
+
+//a singly linked list
+//(sort of special case of a tree)
+
+//a node can only point to a child
+
+// can only have one root
+
+//TREE TERMINOLOGY
+
+//ROOT - The top node in a tree
+//CHILD - A node directly connected to another node when moving away from the root
+//PARENT -  The converse notion of a child
+// SINLINGS - a group of nodes with the same parent
+//LEAF - a node with no children
+//EDGE - the connection between one node and another
