3e4af15de95087315f4bdda30d6158fdf272ed93
jbep <joshuabemartin@gmail.com>
2023-03-02 14:31:35 -0500

    DFS - inorder solution

--- coleSteeleUdemy/section23.js
+++ coleSteeleUdemy/section23.js
@@ -116,8 +116,8 @@ class BinarySearchTree {
     let data = [];
     function traverse(node){
       if(node.left) traverse node.left;
-      if(node.right) traverse node.right;
       data.push(node.value)
+      if(node.right) traverse node.right;
     }
     traverse(this.root);
     return data;
