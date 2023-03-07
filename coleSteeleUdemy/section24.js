90462a43763b525afd5026acc5eedf9827f58be5
jbep <joshuabemartin@gmail.com>
2023-03-07 13:14:07 -0500

    intro to heaps

--- coleSteeleUdemy/section24.js
+++ coleSteeleUdemy/section24.js
@@ -12,5 +12,24 @@
 
 //what is a binary heap?
 //very similar to a binary search tree, but with some diferent rules!
-//in a maxBinaryHeap, parent nodes are always larger than child nodes. In a MinBinaryHeap,
-//parent nodes are always smaller than child nodes
+
+//in a maxBinaryHeap, parent nodes are always larger than child nodes.
+//In a MinBinaryHeap, parent nodes are always smaller than child nodes
+
+// max has two children
+
+//MAX BINARY HEAP
+
+//each parent has at most two child nodes
+//the value of each parent node is always greater than it's child nodes
+//in a max binary heap the parent is greater than the children, but there are no guarantees
+//between sibling nodes
+//a binary heap is as compact as possible. All the children of each node are as full
+//as they can be and left children are filled out first
+
+//max binary
+//left and right children will be smaller
+
+//min binary
+//left and right are lager than parent
+//no relationship between siblings
