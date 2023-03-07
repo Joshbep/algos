d72280a8fa4f5443136b1163a03d06e1d0047a17
jbep <joshuabemartin@gmail.com>
2023-03-07 13:06:13 -0500

    what is a binaryb heap

--- coleSteeleUdemy/section24.js
+++ coleSteeleUdemy/section24.js
@@ -3,3 +3,14 @@
 //heaps are another category of trees
 
 //focusing on Binary heap
+
+//objectives
+//define what a binary heap is
+//compare and contrast min and max heaps
+//implement basic methods on heaps
+//understand whee heaps are used in the real world
+
+//what is a binary heap?
+//very similar to a binary search tree, but with some diferent rules!
+//in a maxBinaryHeap, parent nodes are always larger than child nodes. In a MinBinaryHeap,
+//parent nodes are always smaller than child nodes
