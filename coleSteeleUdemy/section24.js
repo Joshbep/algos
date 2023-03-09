cdbe69c01a5cd9bb161ab6ff6d256028e305ac58
jbep <joshuabemartin@gmail.com>
2023-03-09 15:03:36 -0500

    extract max solution

--- coleSteeleUdemy/section24.js
+++ coleSteeleUdemy/section24.js
@@ -75,8 +75,41 @@
        idx = parentIdx
      }
    }
-   remove(element){
+   extractMax(){
+     const max = this.values[0];
+     const end = this.values.pop()
+     this.values[0] = end;
+     this.sinkDown();
+     return max;
+   }
+   sinkDown(){
+     let idx = 0;
+     const length = this.values.length;
+     const element = this.values[0];
+     while(true){
+       let leftChildIdx = 2 * idx + 1;
+       let rightChildIdx = 2 * idx + 2;
+       let leftChild,rightChild;
+       let swap = null;
 
+       if(leftChildIdx < length){
+         leftChild = this.value[leftChildIdx];
+         if(leftChild > element){
+           swap = leftChildIdx
+         }
+       }
+
+       if(rightChildIdx < length){
+         rightChild = this.values[rightChildIdx];
+         if((swap === null && rightChild > element) ||
+         (swap !== null && rightChild > leftChild)
+       ){
+          swap = rightChildIdx;
+         }
+       }
+
+       if(swap === null) break;
+     }
    }
  }
 
