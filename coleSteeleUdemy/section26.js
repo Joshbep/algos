8028ec09dccb1f53ed173e35b6cabf37176dc828
jbep <joshuabemartin@gmail.com>
2023-03-15 16:44:16 -0400

    graphs section26 objectives

---
+++ coleSteeleUdemy/section26.js
@@ -0,0 +1,8 @@
+//GRAPHS
+
+//objectives
+//explain a graph
+//compare and contrast different types of graphs and their use cases in rewal world
+//implement a grpah using an adjacency list
+//traverse through a graph using BFS and DFS
+//compare and contrast grph traversal algos
