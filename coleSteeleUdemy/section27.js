a90c80f192ab5540092c0cb0697e2561b939bc6d
jbep <joshuabemartin@gmail.com>
2023-03-16 14:09:48 -0400

    intro to graph traversal

--- coleSteeleUdemy/section27.js
+++ coleSteeleUdemy/section27.js
@@ -1,1 +1,12 @@
 //GRAPH TRAVERSAL
+
+//graph traversal uses
+//peer to peer networking
+//web crawlers
+//finding closest matches/recommendations
+//shortest path problems
+  //gps navigation
+  //solving mazes
+  //AI (shortest path to win the game)
+
+//DEPTH FIRST SEARCH
