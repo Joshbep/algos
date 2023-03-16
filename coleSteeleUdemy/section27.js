8327d6008ee48149942170b02f20797430aa33b8
jbep <joshuabemartin@gmail.com>
2023-03-16 15:35:20 -0400

    depth first intro

--- coleSteeleUdemy/section27.js
+++ coleSteeleUdemy/section27.js
@@ -10,3 +10,40 @@
   //AI (shortest path to win the game)
 
 //DEPTH FIRST SEARCH
+//DFS Pseudocode
+//DFS(vertex):
+  //if the vertex is empty
+    //return base case
+  //add vertex to results list
+  //mark vertex as visited
+  //for each neighbor in vertex's neighbors:
+    //if neighbor is not visited:
+      //recursively call DFS on neighbor
+
+class Graph {
+  constructor(){
+    this.adjacentList = {}
+  }
+  addVertex(vertex){
+    if(!this.adjacentList[vertex]) this.adjacentList[vertex] = [];
+  }
+  addEdge(v1, v2){
+    this.adjacentList[v1].push(v2)
+    this.adjacentList[v2].push(v1)
+  }
+  removeEdge(v1, v2){
+    this.adjacentList[v1] = this.adjacentList[v1].filter(
+      v => v !== v2
+    );
+    his.adjacentList[v2] = this.adjacentList[v2].filter(
+      v => v !== v1
+    );
+  }
+  removeVertex(vertex){
+    while(this.adjacenctList[vertex].length){
+      coonst adjacentVertex = this.adjacentList[vertex].pop();
+      this.removeEdge(vertex, adjacentVertex);
+    }
+    delete this.adjacentList[vertex]
+  }
+}
