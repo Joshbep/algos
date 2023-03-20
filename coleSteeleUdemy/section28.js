cb56b6f1f5220a28113b401f4506de7c71dd4c9e
jbep <joshuabemartin@gmail.com>
2023-03-20 10:55:42 -0400

    intro to section28

--- coleSteeleUdemy/section28.js
+++ coleSteeleUdemy/section28.js
@@ -4,3 +4,22 @@
 //implement weighted graph
 //walk through the steps of DIJKSTR
 //implement using a naive priorty queue
+
+//what is it?
+//one of the most famous and widely used algos around
+//find the shortest path between two vertices ona graph
+//what's the fastest way to get from point A to point B
+
+//Who was he?
+//edsger dijkstra was a dutch programmer, physicist essayist, and all around smarty pants
+
+//he helped to advance the field of computer science from an "art" to an academic discipline
+
+//why is it useful?
+//GPS
+//network routing
+//biology - used to model the spread of viruses among humans
+//airline tickets
+//many other uses
+
+//write  a weighted graph
