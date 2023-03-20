1975a7e5786e150292ab02c23227c65eb5d50039
jbep <joshuabemartin@gmail.com>
2023-03-20 11:00:36 -0400

    built out our weighted graph class

--- coleSteeleUdemy/section28.js
+++ coleSteeleUdemy/section28.js
@@ -23,3 +23,16 @@
 //many other uses
 
 //write  a weighted graph
+
+class WeightedGraph {
+  constrcutor() {
+    this.adjacencyList = {}
+  }
+  addVertex(vertex) {
+    if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+  addEdge(v1, v2, weight) {
+    this.adjacencyList[v1].push[{node:v2, weight}]
+    this.adjacencyList[v2].push[{node:v1, weight}]
+  }
+}
