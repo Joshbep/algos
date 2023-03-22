8c895d175ddbaee0044c90e4058872aa391bd161
jbep <joshuabemartin@gmail.com>
2023-03-22 17:45:42 -0400

    finished course

--- coleSteeleUdemy/section29.js
+++ coleSteeleUdemy/section29.js
@@ -39,16 +39,35 @@
 //storing the results of expensive function calls and returning the cached result when the same inputs occur again
 
 
-function fib(n, memo=[undefined, 1, 1]) {
-  if(memo[n] !== undefined) return memo[n];
-  if(n <= 2) return 1;
-  let res = fib(n-1, memo) + fib(n-2, memo);
-  memo[n] = res;
-  return res;
-}
+// function fib(n, memo=[undefined, 1, 1]) {
+//   if(memo[n] !== undefined) return memo[n];
+//   if(n <= 2) return 1;
+//   let res = fib(n-1, memo) + fib(n-2, memo);
+//   memo[n] = res;
+//   return res;
+// }
 
 //Recursion + Memoization
 //time complexitity of memoized solution
 
 //MUCH BETTER
 //O(N)
+
+//Once again Dynamic Programming
+//a Method for solving a complex problem by breaking it down
+//into a collection of simpler subproblems, solving each of
+//those subproblems just once, and storing their solutions
+
+//Tabulation
+//Storing the result of a previous result in a "table" (usually an array)
+//usually done using iteration
+//better space complexity can be achieved using tabulation
+
+function fib_table(n) {
+  if(n <= 2) return 1;
+  let fibNums = [0, 1, 1];
+  for(let i = 3; i <= n; i++){
+    fibNums[i] = fibNums[i - 1] + fibNums[i - 2];
+  }
+  return fibNums[n];
+}
