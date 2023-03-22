0a34e31db323672a6da4a7e52a466927c60530e7
jbep <joshuabemartin@gmail.com>
2023-03-22 16:29:56 -0400

    optimal sub structure

---
+++ coleSteeleUdemy/section29.js
@@ -0,0 +1,17 @@
+//What is dynmaic programming?
+//a Method for solving a complex problem by breaking it down into a collection of simpler subproblems, solving each of those subproblems just once, and storing their solutions
+
+//where does the name come from?
+//coined by richard bellman
+
+//overlapping subproblems
+//a problem is said to have overlapping subproblems if it can
+//be broken into subproblems which are reused several times
+
+//fibanacci sequence
+//"ever number after the first two is the sum of the two preceding ones"
+//1,1,2,3,5,8,13,21
+
+//optimal substructure
+//a problem is said to have optimal substructure if an optimal solution can be constructured
+//from optimal solutions of its subproblems
