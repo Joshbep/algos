9aae86e7c19161add35a17c91ac68cfd5931c505
jbep <joshuabemartin@gmail.com>
2023-03-23 17:04:52 -0400

    twoSum solution

--- grind75/week1_algos/twoSum.js
+++ grind75/week1_algos/twoSum.js
@@ -1,17 +1,17 @@
-var twoSum = function(nums, target) {
-    let newArr = []
-    for(let i = 0; i < nums.length; i++){
-        for(let j = i+1; j < nums.length; j++) {
-            if(nums[i] + nums[j] === target) {
-                newArr.push(i)
-                newArr.push(j)
-            }
-        }
-    }
-    return newArr
-};
-
-console.log(twoSum([2,1,3,4], 3))
+// var twoSum = function(nums, target) {
+//     let newArr = []
+//     for(let i = 0; i < nums.length; i++){
+//         for(let j = i+1; j < nums.length; j++) {
+//             if(nums[i] + nums[j] === target) {
+//                 newArr.push(i)
+//                 newArr.push(j)
+//             }
+//         }
+//     }
+//     return newArr
+// };
+//
+// console.log(twoSum([2,1,3,4], 3))
 
 var twoSum = function(nums, target) {
   let hash = {};
@@ -27,3 +27,5 @@ var twoSum = function(nums, target) {
     }
   }
 };
+
+console.log(twoSum([3,2,4,5], 5))
