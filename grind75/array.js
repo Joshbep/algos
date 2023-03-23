a07aa0b5777f106509b3769cd5ae289790cb0d29
jbep <joshuabemartin@gmail.com>
2023-03-23 15:43:46 -0400

    beg of array notes for grind75

---
+++ grind75/array.js
@@ -0,0 +1,13 @@
+//an array is a data strcuture for storing more than one data item.
+//memory locations are called elements
+//totla number of elements in an array is called length
+
+// An array is a container of elements.
+// Elements have a specific value and data type, like “ABC”, TRUE or FALSE, etc.
+// Each element also has its own index, which is used to access the element.
+
+// Elements are stored at contiguous memory locations.
+// An index is always less than the total number of array items.
+// In terms of syntax, any variable that is declared as an array can store multiple values.
+// Almost all languages have the same comprehension of arrays but have different ways of declaring and initializing them.
+// However, three parts will always remain common in all the initializations, i.e., array name, elements, and the data type of elements.
