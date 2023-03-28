01d7a9ebd3739e569e0865b3694ac5adaff0739f
jbep <joshuabemartin@gmail.com>
2023-03-28 16:39:20 -0400

    some reading on stacks

---
+++ grind75/week1/stack.js
@@ -0,0 +1,14 @@
+//A stack is an abstract data type that supports the operations
+//push (insert a new element on the top of the stack)
+//and pop (remove and return the most recently added element,
+//the element at the top of the stack). As an abstract data type,
+//stacks can be implemented using arrays or singly linked lists.
+
+//stacks and overflows
+//The last in, first out principle (LIFO) refers to the rule
+//that whichever element is the last one to go into the stack
+//will be the very first one that has to go out.
+
+//Conversely, this also means that the first element
+//that you put into the stack will always end up being
+//the last one that goes out.
