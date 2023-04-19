c36ccf5e7a68221b6e68d235dea918b3ffb88fef
jbep <joshuabemartin@gmail.com>
2023-04-19 15:07:02 -0400

    is valid palindrome two pointer soltuion

--- grind75/week1_algos/validPalindrome.js
+++ grind75/week1_algos/validPalindrome.js
@@ -9,3 +9,26 @@ var isPalindrome = function(s) {
     }
 };
 console.log(isPalindrome("A man, a plan, a canal: Panama"))
+
+var isPalindrome = function(s) {
+    let string = s.toLowerCase().replace(/[^a-z0-9]/gi, '');
+    console.log(string)
+    let reverse = string.split("").reverse().join("")
+    if(string === reverse) {
+        return true
+    } else {
+        return false
+    }
+};
+
+var isPalindrome = function(s) {
+    let string = s.toLowerCase().replace(/[^a-z0-9]/gi, '');
+    let left = 0;
+    let right = string.length - 1;
+    while(left < right){
+      if(left !== right) return false
+      left++;
+      right--;
+    }
+    return true;
+};
