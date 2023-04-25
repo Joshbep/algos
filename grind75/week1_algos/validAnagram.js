57d15f45a9fb2c7170dae6b41fdde83623b2e9ea
jbep <joshuabemartin@gmail.com>
2023-04-25 15:15:32 -0400

    solved valid anagram with hashmap

--- grind75/week1_algos/validAnagram.js
+++ grind75/week1_algos/validAnagram.js
@@ -1,3 +1,7 @@
+//time complexity O(n log n)
+//Space complexity O(n)
+
+//1st solution
 var isAnagram = function(s, t) {
     if(s.length !== t.length) return false
     let newS = s.split("").sort().join("")
@@ -8,4 +12,23 @@ var isAnagram = function(s, t) {
     } else {
         return true
     }
+};
+
+//second solution
+var isAnagram = function (s, t) {
+  if(s.length !== t.length) return false
+  let sCount = {};
+  let tCount = {};
+
+  for(let i = 0; i < s.length; i++){
+    if(!sCount[s[i]]) sCount[s[i]] = 0
+    if(!tCount[t[i]]) tCount[t[i]] = 0
+    sCount[s[i]]++
+    tCount[t[i]]++
+  }
+
+  for(ch in sCount){
+    if(sCount[ch] !== tCount[ch]) return false
+  }
+  return true
 };
