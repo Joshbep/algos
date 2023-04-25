f69a1350f3f080f52cb1566a65d4a2cf95eb874e
jbep <joshuabemartin@gmail.com>
2023-04-25 17:16:37 -0400

    updated question onto file

--- grind75/week1_algos/validAnagram.js
+++ grind75/week1_algos/validAnagram.js
@@ -1,7 +1,23 @@
-//time complexity O(n log n)
-//Space complexity O(n)
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+//
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+//
+//
+//
+// Example 1:
+//
+// Input: s = "anagram", t = "nagaram"
+// Output: true
+// Example 2:
+//
+// Input: s = "rat", t = "car"
+// Output: false
+
+
 
 //1st solution
+//time complexity O(n log n)
+//Space complexity O(n)
 var isAnagram = function(s, t) {
     if(s.length !== t.length) return false
     let newS = s.split("").sort().join("")
@@ -15,6 +31,7 @@ var isAnagram = function(s, t) {
 };
 
 //second solution
+//time complexity O(n)
 var isAnagram = function (s, t) {
   if(s.length !== t.length) return false
   let sCount = {};
