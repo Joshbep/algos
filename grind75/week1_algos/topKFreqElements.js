6e0bd38afcdc717be6116afa1c4a5f80707fe4a3
jbep <joshuabemartin@gmail.com>
2023-05-08 18:25:26 -0400

    feat: solves top k frequent elements

--- grind75/week1_algos/topKFreqElements.js
+++ grind75/week1_algos/topKFreqElements.js
@@ -13,17 +13,32 @@ Input: nums = [1], k = 1
 Output: [1]
 */
 
+//given an array of numbers and k which is most req elements
+ //return the kth amount of most freq numbers
+
+ //need to map value as key and frequency as value
+ //need to sort in descending order for freqency
+ //need to set an array for results
+ //loop over array
+ //push kth elements into array
+ //return array
+
+
+//time O(n log n)
+//space O(n)
 var topKFrequent = function(nums, k) {
-    let freqCount = {}
-    nums.forEach((num, i) => {
+    let freqCount = {};
+
+    nums.forEach((num) => {
         if(!freqCount[num]) freqCount[num] = 0
         freqCount[num]++
     })
-    let sorted = Object.entries(freqCount).sort((a,b) => b[1] - a[1]);
 
-    let result = []
+    let sorted = Object.entries(freqCount).sort((a, b) => b[1] - a[1])
+
+    let result = [];
     for(let i = 0; i < k; i++){
-        result.push(Number(sorted[i][0]))
+        result.push(sorted[i][0])
     }
     return result
 };
