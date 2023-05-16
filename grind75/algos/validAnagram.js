2e85631b284d8a6e90ec4163de71e946c4e03d53
jbep <joshuabemartin@gmail.com>
2023-05-16 15:02:05 -0400

    feat: valid anagram re solved leet code problem

--- grind75/algos/validAnagram.js
+++ grind75/algos/validAnagram.js
@@ -33,32 +33,31 @@ var isAnagram = function(s, t) {
 //second solution
 //time complexity O(n)
 
-//we have two string
-//we need to cpmare the strings and check if they have the same characters
-//if the same return true
-//if not the same return false
-//length !== length return false
-//hash = {}
-//hash = {}
-//loop over
-//if not there set to 0
-//if there ++ to show that ch has that number of occurances
-//compare ch or each map
+//we need to compare two strings
+//use hash map to keep count of each letter wihtin each string
+//tCount
+//sCount
+//compare lengths and return false if lenghts aren't the same
+//loop over s string
+//this is how we see if that letter exists in s count or t count
+//then do for in loop to loop over an object
+//for in loop lets you loop over key value pairs in objects
+//compare sCount to tCount if they don't exist return true
 var isAnagram = function(s, t) {
-   //compare lengths
-   if(s.length !== t.length) return false
-   //declare hash maps
-   let sCount = {};
-   let tCount = {};
-   //loop over s since both lengths are the same
-   for(let i =0; i < s.length; i++){
-       if(!sCount[s[i]]) sCount[s[i]] = 0
-       if(!tCount[t[i]]) tCount[t[i]] = 0
-       sCount[s[i]]++
-       tCount[t[i]]++
-   }
-   for(ch in sCount){
-       if(sCount[ch] !== tCount[ch]) return false
-   }
-   return true
-};
+    if(s.length !== t.length) return false
+
+    let sCount = {}
+    let tCount = {}
+
+    for(let i = 0; i < s.length; i++){
+        if(!sCount[s[i]]) sCount[s[i]] = 0
+        if(!tCount[t[i]]) tCount[t[i]] = 0
+        sCount[s[i]]++
+        tCount[t[i]]++
+    }
+
+    for(let ch in sCount){
+        if(sCount[ch] !== tCount[ch]) return false
+    }
+    return true
+}
