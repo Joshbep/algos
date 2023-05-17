a453593f13aeea517ca710941c61a4886a11167e
jbep <joshuabemartin@gmail.com>
2023-05-17 17:45:30 -0400

    feat: solutino for count of positive numbers and sum of nagative numbers code wars

--- codeWars/count-of-positive-sum-of-negatives.js
+++ codeWars/count-of-positive-sum-of-negatives.js
@@ -11,15 +11,19 @@ For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should r
 
 function countPositivesSumNegatives(input) {
   // your code here
-  let positiveCount = 0
-  let negativeCount = 0
 
-  for(let i = 0; i < input.length; i++){
-    if(input[i] > 0) {
-      positiveCount+= 1
-    } else {
-      negativeCount+= input[i]
+  let array = [0, 0]
+
+  if (input === null || input.length < 1) {
+        return [];
     }
-  }
-  return [positiveCount, negativeCount]
+
+    for(let i = 0; i < input.length; i++) {
+        if(input[i] > 0) {
+            array[0] += 1
+      } else {
+            array[1] += input[i];
+      }
+    }
+    return array;
 }
