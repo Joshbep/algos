861d865f85ce24c382a5b20faf5fbddb613068a8
jbep <joshuabemartin@gmail.com>
2023-06-01 14:56:42 -0400

    feat: time conversion hacker rank problem solution

---
+++ hackerRank/timeConversion.js
@@ -0,0 +1,99 @@
+/*
+Given a time in -hour AM/PM format, convert it to military (24-hour) time.
+
+Note: - 12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock.
+- 12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock.
+
+Example
+
+
+Return '12:01:00'.
+
+
+Return '00:01:00'.
+
+Function Description
+
+Complete the timeConversion function in the editor below. It should return a new string representing the input time in 24 hour format.
+
+timeConversion has the following parameter(s):
+
+string s: a time in  hour format
+Returns
+
+string: the time in  hour format
+Input Format
+
+A single string  that represents a time in -hour clock format (i.e.:  or ).
+
+Constraints
+
+All input times are valid
+Sample Input
+
+07:05:45PM
+Sample Output
+
+19:05:45
+*/
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'timeConversion' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+function timeConversion(s) {
+  const timeArr = s.slice(0, 8).split(':');
+  let hours = parseInt(timeArr[0], 10);
+  const minutes = timeArr[1];
+  const seconds = timeArr[2];
+  const modifier = s.slice(8, 10);
+
+  if (modifier === 'PM' && hours !== 12) {
+    hours = (hours + 12) % 24;
+  } else if (modifier === 'AM' && hours === 12) {
+    hours = 0;
+  }
+
+  const formattedHours = hours.toString().padStart(2, '0');
+
+  return `${formattedHours}:${minutes}:${seconds}`;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const s = readLine();
+
+    const result = timeConversion(s);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
